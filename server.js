480c90e53619d765d9e8e5487c890a2774f9bf25
Bofta <montassar367@gmail.com>
2024-07-25 01:53:54 +0200

    Express Apis

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import exp from 'constants';
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/posts', posts);
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
