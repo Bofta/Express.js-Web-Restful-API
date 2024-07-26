f7f18183395bc47ebbdf83e06c252bf2853eab67
Bofta <montassar367@gmail.com>
2024-07-26 02:10:15 +0200

    Express & Posts API

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,19 @@
-// const logger = () => (req, res, next) => { 
-//     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-//     res.JSON.stringify('Hello I AM THE ERROR');
-//     next();
-// }
+import colors from 'colors';
 
-// export default logger;
+const logger = () => (req, res, next) => {
+    const methodColors = {
+        GET: 'green',
+        POST: 'blue',
+        PUT: 'yellow',
+        DELETE: 'red'
+    }
+
+    const colors = methodColors[req.method] || white;
+
+
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)['red'];
+    res.JSON.stringify('Hello I AM THE ERROR');
+    next(res.status);
+}
+
+export default logger;
--- routes/posts.js
+++ routes/posts.js
@@ -1,84 +1,21 @@
 import express from 'express';
+import { createPost, deletePost, getPost, getPosts, updatePost } from '../controllers/postController.js';
 const router = express.Router();
 
 
-
-// Posts Json objects
-let posts = [
-    {id: 1, title: 'Post one'},
-    {id: 2, title: 'Post two'},
-    {id: 3, title: 'Post three'}
-];
-
-
 // Get all posts
-router.get('/', (req, res) => {
-    const limit = parseInt(req.query.limit);
-
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(posts.slice(0, limit));
-    } 
-    
-    res.status(200).json(posts);
-});
+router.get('/', getPosts);
 
 // Get single post
-router.get('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id );
-    
-    if (!post) {
-        const error = new Error(`A post with the id of ${id} was not found`);
-        return next(error);
-    } 
-    res.status(200).json(post);
-    
-});
+router.get('/:id', getPost);
 
 // Create new post
-router.post('/', (req, res) => {
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body.title
-    };
-
-    if(!newPost.title) {
-        return res.status(400).json({msg: 'Please include a title'});
-    }
-
-    posts.push(newPost);
-
-    res.status(201).json(posts);
-});
+router.post('/', createPost);
 
 // Update Post
-router.put('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id); 
-
-    if (!post) {
-        return res.
-        status(404).
-        json({msg: `A post with if the id of ${id} was not found`});
-    }
-
-    post.title = req.body.title;
-    res.status(200).json(posts);
-});
+router.put('/:id', updatePost);
 
 // Delete Post
-router.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id); 
-
-    if (!post) {
-        return res.status(404).json({msg: `A post with if the id of ${id} was not found`});
-    }
-
-    posts = posts.filter((post) => post.id !== id);
-    res.status(200).json(posts);
-});
-
-
+router.delete('/:id', deletePost);
 
 export default router;
